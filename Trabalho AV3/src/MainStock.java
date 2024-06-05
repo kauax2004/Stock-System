10c474a498f4be741f67155d0b73f7c0f1bc73c2
Pedro Kauã Fernandes Vieira Castro de Vasconcelos <pedro.kaua910@gmail.com>
2024-06-05 15:29:11 -0300

    Screen Creation

--- Trabalho AV3/src/MainStock.java
+++ Trabalho AV3/src/MainStock.java
@@ -1,10 +1,10 @@
+import interface1.Screen;
+
 public class MainStock {
 
 	public static void main(String[] args) {
-		//Account a = new Account("fodase@gmail.com","PReto123");
-		//Account a1 = new Account("fodaseskjbvskj@gmail.com","Branco123");
 		
+		new Screen();
 		
 	}
-
 }
